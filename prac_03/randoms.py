16b8d554ffe5aed772aa3011302551c111719688
Stephen Yamashita <jamal.yamashita@my.jcu.edu.au>
2025-06-06 22:08:19 +1000

    completed randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,18 @@
+# What did you see on line 1?
+#
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number that could be seen is 5 and the largest was 20.
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number was 3 and the largest was 10.
+# Could line 2 have produced a 4?
+# No it could not produce a 4 as the ranges was between 3 and
+# 10 which 4 is well within the range, it was increments of 2 making it impossible
+# to get 4 since 2 after 3 is 5.
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+# Largest I have seen was 5.069151282565553 and smallest was 2.601465069442386
+
+import random
+
+print(random.randint(0, 100))
