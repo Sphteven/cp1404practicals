ff18118d239e7ee245f0217ad96d019d229a81be
Stephen Yamashita <jamal.yamashita@my.jcu.edu.au>
2025-06-12 21:43:52 +1000

    Refactored print_report to standard

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -15,14 +15,14 @@ def main():
 
     print("\nIncome Report\n-------------")
     total = 0
-    print_report(incomes, number_of_months, total)
-
-
-def print_report(incomes, number_of_months, total):
     for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print_report(income, month, total)
+
+
+def print_report(income, month, total):
+    print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
 
 main()
