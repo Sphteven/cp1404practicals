0003947a7ef4ab1ca91658d4ee21c844d96fb3e7
Stephen Yamashita <jamal.yamashita@my.jcu.edu.au>
2025-06-12 21:10:47 +1000

    Complete warm up tasks by changing elements in list

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -10,3 +10,17 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # "3" in numbers = False
 # numbers + [6, 5, 3] = error TypeError
 
+print(numbers[0])
+print(numbers[-1])
+print(numbers[3])
+print(numbers[:-1])
+print(numbers[3:4])
+print(5 in numbers)
+print(7 in numbers)
+print("3" in numbers)
+print(numbers + [6, 5, 3])
+
+numbers[0] = "ten"
+numbers[-1] = 1
+print(9 in numbers)
+print(numbers)
