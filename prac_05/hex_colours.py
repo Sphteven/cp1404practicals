f761dac502f2c6db1c79dff0f073cd06f566dc5b
Stephen Yamashita <jamal.yamashita@my.jcu.edu.au>
2025-06-21 14:17:47 +1000

    Complete prac 5 hex colours exercise

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,26 @@
+"""
+CP1404/CP5632 Practical
+Hex codes in dictionary
+"""
+
+# "QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+#                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"
+
+HEX_NAME_TO_CODE = {"absolute zero": "#0048ba", "acid green": "#b0bf1a", "aliceblue": "#f0f8ff",
+                    "amber": "#9966cc", "amethyst": "#9966cc", "aqua": "#00ffff",
+                    "azure1": "#f0ffff", "baby blue": "#89cff0", "baby pink": "#f4c2c2",
+                    "banana yellow": "#ffe135"}
+print(HEX_NAME_TO_CODE)
+
+for i in HEX_NAME_TO_CODE:
+    print(f"{i:<13} is {HEX_NAME_TO_CODE[i]}")
+
+hex_name = input("Enter hex name: ").lower()
+while hex_name != "":
+    try:
+        hex_code = HEX_NAME_TO_CODE[hex_name].lower()
+        print(hex_name, " hex code is", hex_code)
+    except KeyError:
+        print("Invalid hex name")
+    hex_name = input("Enter hex name: ").lower()
+
