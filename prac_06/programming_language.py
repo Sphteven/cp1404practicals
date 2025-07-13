a1371c9e5751114a70cb34347a923039917fbcfa
Stephen Yamashita <jamal.yamashita@my.jcu.edu.au>
2025-07-13 16:57:58 +1000

    Add docstrings to programming_language.py

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -8,14 +8,23 @@ Actual:    minutes
 
 
 class ProgrammingLanguage:
+    """Represent a programming language object."""
     def __init__(self, name, typing, reflection, year):
+        """Initialise a programming language instance.
+        name: string, The name of the programming language.
+        typing: string, Programming language type.
+        reflection: boolean, Programming language is reflection or not.
+        year: integer, The year of the programming language was released.
+        """
         self.name = name.title()
         self.typing = typing.title()
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
+        """Determine if a programming language is dynamic."""
         return self.typing == 'Dynamic'
 
     def __str__(self):
+        """Provide object details in a neat format."""
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
