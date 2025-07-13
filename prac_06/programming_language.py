1d5e612f2f11f75043be3a76cd894766c62aee17
Stephen Yamashita <jamal.yamashita@my.jcu.edu.au>
2025-07-13 16:52:56 +1000

    Complete progamming_language task

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -9,11 +9,13 @@ Actual:    minutes
 
 class ProgrammingLanguage:
     def __init__(self, name, typing, reflection, year):
-        self.name = name
-        self.typing = typing.lower()
+        self.name = name.title()
+        self.typing = typing.title()
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
-        return self.typing == 'dynamic'
+        return self.typing == 'Dynamic'
 
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
