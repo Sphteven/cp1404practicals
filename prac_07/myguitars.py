10fcc84670bb16b664d82ffafb731a0f4e77b8c8
Stephen Yamashita <jamal.yamashita@my.jcu.edu.au>
2025-07-21 19:51:45 +1000

    Complete rest of MORE GUITARS task

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -3,6 +3,36 @@ from guitar import Guitar
 
 def main():
     FILENAME = "guitars.csv"
+    guitars = load_from_file(FILENAME)
+    guitars.sort()
+
+    print("")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        print(f"{new_guitar} added.")
+        name = input("Name: ")
+
+    output_to_file(FILENAME, guitars)
+    print("These files have been added: ")
+    list_guitars(guitars)
+
+
+def output_to_file(FILENAME, guitars):
+    with open(FILENAME, "w") as guitars_file:
+        for guitar in guitars:
+            guitars_file.write(f"{guitar.name},{guitar.year},{guitar.cost}\n")
+
+
+def list_guitars(guitars):
+    for guitar in guitars:
+        print(guitar)
+
+
+def load_from_file(FILENAME):
     guitars = []
     in_file = open(FILENAME, "r")
     for line in in_file:
@@ -12,9 +42,7 @@ def main():
         guitar = Guitar(parts[0], year, cost)
         guitars.append(guitar)
     in_file.close()
-    guitars.sort()
-    for guitar in guitars:
-        print(guitar)
+    return guitars
 
 
 main()
