9f168f8104850d707aa5cc1ee3bfdb2fc9b9c4db
Stephen Yamashita <jamal.yamashita@my.jcu.edu.au>
2025-07-21 20:11:05 +1000

    Initial commit to PROJECT MANAGEMENT task

---
+++ prac_07/project_management.py
@@ -0,0 +1,18 @@
+"""
+Estimated time: 40min
+Actual time:
+
+"""
+def main():
+    """Run through the menu until user decides to quit."""
+
+    MENU = "- (L)oad projects \n"\
+           "- (S)ave projects \n" \
+           "- (D)isplay projects  \n"\
+           "- (F)ilter projects by date\n"\
+           "- (A)dd new project  \n"\
+           "- (U)pdate project \n"\
+           "- (Q)uit"
+
+
+main()
