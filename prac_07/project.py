b2e887ccfc0805411b7fc69bae63bc1bc3d2e260
Stephen Yamashita <jamal.yamashita@my.jcu.edu.au>
2025-07-21 21:29:01 +1000

    Add display functionality, adjust Project class __str__ formatting and attributes for the class

--- prac_07/project.py
+++ prac_07/project.py
@@ -3,11 +3,11 @@ class Project:
         self.name = name
         self.start_date = start_date
         self.priority = priority
-        self.cost_estimate = cost_estimate
-        self.completion_percentage = completion_percentage
+        self.cost_estimate = float(cost_estimate)
+        self.completion_percentage = int(completion_percentage)
 
     def __str__(self):
-        return f"{self.name}, start: {self.start_date}, priority: {self.priority}, estimate: ${self.cost_estimate}, completion: {self.completion_percentage}%"
+        return f"{self.name}, start: {self.start_date}, priority: {self.priority}, estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%"
 
     def __lt__(self, other):
         return self.priority < other.priority
