24e8a5838a85deaa9ac0df71e17f7c4f427023cf
Stephen Yamashita <jamal.yamashita@my.jcu.edu.au>
2025-07-23 13:15:11 +1000

    Complete project_management task

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,9 +1,11 @@
+
 """
 Estimated time: 40min
 Actual time: 2 hours 30 min
 
 """
 
+import datetime
 from project import Project
 
 
@@ -24,7 +26,12 @@ def main():
     choice = input(MENU).upper()
     while choice != "Q":
         if choice == "L":
-            print("yes")
+            file_name = input("Enter the name you will like to load:")
+            file_name = file_name + ".txt"
+            temp_projects = load_from_file(file_name)
+            if temp_projects is not None:
+                projects = temp_projects
+                print(f"{len(projects)} projects loaded")
         elif choice == "S":
             file_name = input("Enter a file name you would like to save to: ")
             while file_name == "":
@@ -42,11 +49,27 @@ def main():
                 if project.is_complete() is True:
                     print(project)
         elif choice == "F":
-            print("yest")
+            date_choice = input("Show projects after date (d/m/yyyy): ")  # e.g., "30/9/2022"
+            date = datetime.datetime.strptime(date_choice, "%d/%m/%Y").date()
+            for project in projects:
+                project_date = datetime.datetime.strptime(project.start_date, "%d/%m/%Y").date()
+                if date < project_date:
+                    print(project)
         elif choice == "A":
             projects.append(add_new_project())
         elif choice == "U":
-            print("yest")
+            for i, project in enumerate(projects):
+                print(f"{i} {project}")
+            project_choice = int(input("Project choice: "))
+            if project_choice != "":
+                try:
+                    print(projects[project_choice])
+                    new_percentage = int(input("New percentage: "))
+                    new_priority = int(input("New priority: "))
+                    projects[project_choice].completion_percentage = new_percentage
+                    projects[project_choice].priority = new_priority
+                except IndexError:
+                    print("Invalid choice")
         else:
             print("Invalid input")
         choice = input(MENU).upper()
@@ -59,7 +82,7 @@ def add_new_project():
     priority = input("Priority: ")
     cost = input("Cost estimate: ")
     percent_complete = input("Percent complete: ")
-    new_project = Project(name,start_date,priority,cost,percent_complete)
+    new_project = Project(name, start_date, priority, cost, percent_complete)
     return new_project
 
 
@@ -76,14 +99,17 @@ def save_to_file(file_name, projects):
 def load_from_file(file_name):
     """Load projects from specified file."""
     projects = []
-    with open(file_name, "r") as file:
-        file.readline()  # To step over the title in the textfile
-        for line in file:
-            line = line.strip()
-            parts = line.split("\t")
-            project = Project(parts[0], parts[1], parts[2], parts[3], parts[4])
-            projects.append(project)
-    return projects
+    try:
+        with open(file_name, "r") as file:
+            file.readline()  # To step over the title in the textfile
+            for line in file:
+                line = line.strip()
+                parts = line.split("\t")
+                project = Project(parts[0], parts[1], parts[2], parts[3], parts[4])
+                projects.append(project)
+        return projects
+    except FileNotFoundError:
+        print("File does not exist")
 
 
 main()
