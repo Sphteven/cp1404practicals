d12b19f560113a0a13e87262675c0738ba22abf9
Stephen Yamashita <jamal.yamashita@my.jcu.edu.au>
2025-08-03 17:15:23 +1000

    Complete taxi_simulator task

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -9,9 +9,9 @@ def main():
              SilverServiceTaxi("Hummer", 200, 4)]
     total_bill = 0
     print("Let's drive!")
-    user_choice = input(MENU)
-    while user_choice != "q":
-        if user_choice == "c":
+    user_choice = input(MENU).upper()
+    while user_choice != "Q":
+        if user_choice == "C":
             print("Taxis available: ")
             list_taxis(taxis)
             chosen_taxi = input("Choose taxi: ")
@@ -20,19 +20,23 @@ def main():
             except IndexError and ValueError:
                 print("Invalid taxi choice")
 
-        elif user_choice == "d":
+        elif user_choice == "D":
             if current_taxi is not None:
                 try:
                     drive_taxi(current_taxi)
-                    total_bill += current_taxi.get_fare()
+                    total_bill = get_total_bill(taxis)
                 except ValueError:
                     print("Invalid distance")
             else:
                 print("You need to choose a taxi before you can drive")
         else:
             print("Invalid choice")
+
         print(f"Bill to date: ${total_bill:.2f}")
-        user_choice = input(MENU)
+        user_choice = input(MENU).upper()
+    print(f"Total trips cost: ${total_bill:.2f}")
+    print("Taxis are now:")
+    list_taxis(taxis)
 
 
 def drive_taxi(current_taxi):
@@ -44,6 +48,14 @@ def drive_taxi(current_taxi):
 def list_taxis(taxis):
     for i, taxi in enumerate(taxis):
         print(f"{i} - {taxi}")
+
+
+def get_total_bill(taxis):
+    total_bill = 0
+    for taxi in taxis:
+        if taxi.current_fare_distance != 0:
+            total_bill += taxi.get_fare()
+    return total_bill
 
 
 main()
