88b55f1f19d343c6b2a5b9bdf04aa0d1b3cc66f0
Stephen Yamashita <jamal.yamashita@my.jcu.edu.au>
2025-08-03 16:11:38 +1000

    Add exception handling for choosing taxi

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -3,20 +3,28 @@ from taxi import Taxi
 
 
 def main():
-    MENU = "q)uit, c)hoose taxi, d)rive"
+    MENU = "q)uit, c)hoose taxi, d)rive \n"
     current_taxi = None
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
              SilverServiceTaxi("Hummer", 200, 4)]
+    total_bill = 0
     print("Let's drive!")
     user_choice = input(MENU)
     while user_choice != "q":
         if user_choice == "c":
+            print("Taxis available: ")
             list_taxis(taxis)
+            chosen_taxi = input("Choose taxi: ")
+            try:
+                current_taxi = taxis[int(chosen_taxi)]
+            except IndexError and ValueError:
+                print("Invalid taxi choice")
 
         elif user_choice == "d":
             return
         else:
             print("Invalid choice")
+        print(f"Bill to date: ${total_bill}")
         user_choice = input(MENU)
 
 
