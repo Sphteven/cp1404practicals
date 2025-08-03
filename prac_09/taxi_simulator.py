05fc675a31ee15ca3bf7c5efd59c19c29766e106
Stephen Yamashita <jamal.yamashita@my.jcu.edu.au>
2025-08-03 16:05:13 +1000

    Add main structure and choose decision structure

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,24 @@
+from silver_service_taxi import SilverServiceTaxi
+from taxi import Taxi
+
+
+def main():
+    MENU = "q)uit, c)hoose taxi, d)rive"
+    current_taxi = None
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
+    print("Let's drive!")
+    user_choice = input(MENU)
+    while user_choice != "q":
+        if user_choice == "c":
+            for i, taxi in enumerate(taxis):
+                print(f"{i} - {taxi}")
+
+        elif user_choice == "d":
+            return
+        else:
+            print("Invalid choice")
+        user_choice = input(MENU)
+
+
+main()
